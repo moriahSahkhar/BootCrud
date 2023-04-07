9b96c7d7f8ddcebcfcd8ba1cbef1046e22d33e0c
Moya <moriah.s1234@gmail.com>
2023-04-07 17:58:45 +0530

    Second Commit

---
+++ src/main/java/model/User.java
@@ -0,0 +1,29 @@
+package model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "f_name", nullable = false)
+    private String fname;
+
+    @Column(name = "l_name", nullable = false)
+    private String lname;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+}
