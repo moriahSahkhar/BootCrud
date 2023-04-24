08408a46bb2116b58cfaaacc9cdea6ece48cf962
Kernold <mr.kernold@gmail.com>
2023-04-24 11:36:38 +0530

    Swagger OpenUI Implemented

--- src/main/java/org/SpringBootCrud/BootCrud/controller/UserController.java
+++ src/main/java/org/SpringBootCrud/BootCrud/controller/UserController.java
@@ -1,5 +1,8 @@
 package org.SpringBootCrud.BootCrud.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.SpringBootCrud.BootCrud.Exception.ErrorDetails;
@@ -16,6 +19,10 @@ import org.springframework.web.context.request.WebRequest;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Tag(
+        name = "CRUD REST APIs for User Resource",
+        description = "Create User, Update User, Get User, Get All Users, Delete User"
+)
 @RestController
 @AllArgsConstructor
 @RequestMapping("api/users")
@@ -23,26 +30,67 @@ public class UserController {
 
     private UserServiceImpl userService;
 
+    @Operation(
+            summary = "Create User Rest API",
+            description = "Create User Rest API is used to save user in the database"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "HTTP STATUS 201 CREATED"
+    )
     @PostMapping
     public ResponseEntity<UserDto> createUser(@RequestBody @Valid UserDto user){
         return new ResponseEntity<>(userService.createUser(user), HttpStatus.CREATED);
     }
 
+    @Operation(
+            summary = "Get User Rest API",
+            description = "Get User Rest API is used to get a single user from the database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP STATUS 200 SUCCESS"
+    )
     @GetMapping("{id}")
     public ResponseEntity<UserDto> getUserById(@PathVariable Long id){
         return new ResponseEntity<>(userService.getUserById(id), HttpStatus.OK);
     }
+
+    @Operation(
+            summary = "Get all User Rest API",
+            description = "Get all User Rest API is used to get all users from the database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP STATUS 200 SUCCESS"
+    )
     @GetMapping
     public ResponseEntity<List<UserDto>> getAllUsers(){
         return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "Put User Rest API",
+            description = "Put User Rest API is used to update user in the database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP STATUS 200 SUCCESS"
+    )
     @PutMapping("{id}")
     public ResponseEntity<UserDto> updateUser(@PathVariable("id") Long id,@RequestBody @Valid UserDto userDto){
         userDto.setId(id);
         return new ResponseEntity<>(userService.updateUser(userDto), HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "Delete User Rest API",
+            description = "Delete User Rest API is used to delete user from the database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP STATUS 200 SUCCESS"
+    )
     @DeleteMapping("{id}")
     public ResponseEntity<String> deleteUserById(@PathVariable("id") Long id){
 
--- src/main/java/org/SpringBootCrud/BootCrud/dto/UserDto.java
+++ src/main/java/org/SpringBootCrud/BootCrud/dto/UserDto.java
@@ -1,5 +1,6 @@
 package org.SpringBootCrud.BootCrud.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
@@ -7,6 +8,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Schema(
+        description = "User DTO model information"
+)
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
@@ -14,12 +18,21 @@ import lombok.Setter;
 public class UserDto {
     private Long id;
 
+    @Schema(
+            description = "User First Name"
+    )
     @NotEmpty(message = "First Name should not be null or empty")
     private String fname;
 
+    @Schema(
+            description = "User Last Name"
+    )
     @NotEmpty(message = "Last Name should not be null or empty")
     private String lname;
 
+    @Schema(
+            description = "User Email"
+    )
     @NotEmpty(message = "Email should not be null or empty")
     @Email(message = "Email Address Should be valid")
     private String email;
